4725085e97feee42a31d936bf9f3b8176dd0add3
Michael Dyer <michael@416studios.co.uk>
2018-03-10 23:06:56 +0000

    Initial commit add files

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <TextContent/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import TextContent from './components/TextContent'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TextContent
+  }
+}
+</script>
+
+<style lang="scss">
+  @font-face {
+    font-family: 'sourcesans';
+    src: url('./assets/SourceSansVariable-Roman.ttf') format('truetype-variations'), url('./assets/SourceSansVariable-Roman.ttf') format('truetype');
+  }
+  @import './scss/index.scss';
+  html {
+    @include nls-html(16);
+  }
+  body{
+    @include nls-body("rem");
+    font-family: sourcesans;
+    margin: 0;
+  }
+  h1 {
+    @include nls-heading(3, "rem");
+  }
+  h2 {
+    @include nls-heading(2, "rem");
+  }
+  h3 {
+    @include nls-heading(1, "rem");
+  }
+  h4 {
+    @include nls-heading(0, "rem");
+    font-weight: 400;
+  }
+  p, ul, ol, pre, table, blockquote {
+    @include nls-margins();
+  }
+
+</style>
---
+++ src/components/TextContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1><a id="Kitsch_retro_0"></a>Kitsch retro</h1>
+    <h2><a id="Typewriter_delectus_cred_2"></a>Typewriter delectus cred</h2>
+    <p>Bushwick Schlitz. Est Shoreditch small batch, dolor Schlitz sapiente twee stumptown ex. Duis Carles pickled, cornhole Thundercats</p>
+    <p>McSweeney’s minim PBR vegan Tumblr irony. Kogi eu Thundercats, sed scenester before they sold out et aesthetic. Elit cred Vice ethical pickled sartorial. Stumptown roof party freegan High Life vero, ea sed minim meggings.</p>
+    <h3><a id="Truffaut_disrupt_sartorial_deserunt_dfgkj_lgjdfgd_d_gldfkj_8"></a>Truffaut disrupt sartorial deserunt df;gkj lgjdfgd d gldfkj</h3>
+    <p>Cosby sweater plaid shabby chic kitsch pour-over ex. Try-hard fanny pack mumblecore cornhole cray scenester. Assumenda narwhal occupy, Blue Bottle nihil culpa fingerstache. Meggings kogi vinyl meh, food truck banh mi Etsy magna 90’s duis typewriter banjo organic leggings Vice.</p>
+    <h4><a id="Fingerstache_nesciunt_lomo_nostrud_hoodie_12"></a>Fingerstache nesciunt lomo nostrud hoodie</h4>
+    <ul>
+      <li>Roof party put a bird on it incididunt sed umami craft beer cred.</li>
+      <li>Carles literally normcore, Williamsburg Echo Park fingerstache photo booth twee keffiyeh chambray whatever.</li>
+      <li>Scenester High Life Banksy, proident master cleanse tousled squid sriracha ad chillwave post-ironic retro.</li>
+    </ul>
+    <p>Laboris selfies occaecat umami, forage Tumblr American Apparel. Retro Terry Richardson culpa id swag polaroid Intelligentsia American Apparel eu, esse non post-ironic fugiat master cleanse. Direct trade gluten-free blog, fanny pack cray id Helvetica freegan. Disrupt aliqua Brooklyn church-key lo-fi dreamcatcher.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextContent',
+  data () {
+    return {
+      msg: 'Initial simple content'
+    }
+  }
+}
+</script>
